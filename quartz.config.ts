d8e2f36fffd892c83361732635165392b4084c36
TraNzeM <ivanzyablov@gmail.com>
2025-07-24 21:56:41 +0300

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "https://tranzem.github.io/notes",
+    baseUrl: "https://tranzem.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
