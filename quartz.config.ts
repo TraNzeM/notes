0eda8fd17496b800ee1d310f60c1ac57800fe2dd
TraNzeM <ivanzyablov@gmail.com>
2025-07-25 00:20:35 +0300

    Update quartz.config.ts
    ignore tags

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "tranzem.github.io",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "templates", ".obsidian", "tags"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
